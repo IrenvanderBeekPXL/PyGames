2cfcff88cca7f819eb244cc39572c438fdd32267
IrenvanderBeekPXL <Iren.vanderBeek@student.pxl.be>
2020-11-13 15:14:06 +0100

    started memory: kaarten geschud

---
+++ memory/main.py
@@ -0,0 +1,21 @@
+from secrets import randbelow
+def schud_kaarten():
+    nog_beschikbare_kaarten = [["K2", "K3", "K4", "K5", "K6", "K7", "K8", "K9", "K10", "KB", "KQ", "KK", "KA"],
+                               ["S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "SB", "SQ", "SK", "SA"],
+                               ["R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10", "RB", "RQ", "RK", "RA"],
+                               ["H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "HB", "HQ", "HK", "HA"],
+                               ["K2", "K3", "K4", "K5", "K6", "K7", "K8", "K9", "K10", "KB", "KQ", "KK", "KA"],
+                               ["S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "SB", "SQ", "SK", "SA"],
+                               ["R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10", "RB", "RQ", "RK", "RA"],
+                               ["H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "HB", "HQ", "HK", "HA"]]
+    spelbord = [[], [], [], [], [], [], [], []]
+    for i in range(13):
+        for j in range(8):
+            shape = randbelow(8)
+            while len(nog_beschikbare_kaarten[shape]) == 0:
+                shape = randbelow(8)
+            level = randbelow(len(nog_beschikbare_kaarten[shape]))
+            spelbord[j].append(nog_beschikbare_kaarten[shape].pop(level))
+    return spelbord
+
+print(schud_kaarten())
