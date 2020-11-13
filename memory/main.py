dd662b8d0c62d531ca019b60249475c71a6ffd3e
IrenvanderBeekPXL <Iren.vanderBeek@student.pxl.be>
2020-11-13 17:01:02 +0100

    debugd invisible bug

--- memory/main.py
+++ memory/main.py
@@ -1,13 +1,13 @@
 from secrets import randbelow
 def schud_kaarten():
-    nog_beschikbare_kaarten = [["K2", "K3", "K4", "K5", "K6", "K7", "K8", "K9", "K10", "KB", "KQ", "KK", "KA"],
-                               ["S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "SB", "SQ", "SK", "SA"],
-                               ["R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10", "RB", "RQ", "RK", "RA"],
-                               ["H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "HB", "HQ", "HK", "HA"],
-                               ["K2", "K3", "K4", "K5", "K6", "K7", "K8", "K9", "K10", "KB", "KQ", "KK", "KA"],
-                               ["S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "SB", "SQ", "SK", "SA"],
-                               ["R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10", "RB", "RQ", "RK", "RA"],
-                               ["H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "HB", "HQ", "HK", "HA"]]
+    nog_beschikbare_kaarten = [["K2 ", "K3 ", "K4 ", "K5 ", "K6 ", "K7 ", "K8 ", "K9 ", "K10", "KB ", "KQ ", "KK ", "KA "],
+                               ["S2 ", "S3 ", "S4 ", "S5 ", "S6 ", "S7 ", "S8 ", "S9 ", "S10", "SB ", "SQ ", "SK ", "SA "],
+                               ["R2 ", "R3 ", "R4 ", "R5 ", "R6 ", "R7 ", "R8 ", "R9 ", "R10", "RB ", "RQ ", "RK ", "RA "],
+                               ["H2 ", "H3 ", "H4 ", "H5 ", "H6 ", "H7 ", "H8 ", "H9 ", "H10", "HB ", "HQ ", "HK ", "HA "],
+                               ["K2 ", "K3 ", "K4 ", "K5 ", "K6 ", "K7 ", "K8 ", "K9 ", "K10", "KB ", "KQ ", "KK ", "KA "],
+                               ["S2 ", "S3 ", "S4 ", "S5 ", "S6 ", "S7 ", "S8 ", "S9 ", "S10", "SB ", "SQ ", "SK ", "SA "],
+                               ["R2 ", "R3 ", "R4 ", "R5 ", "R6 ", "R7 ", "R8 ", "R9 ", "R10", "RB ", "RQ ", "RK ", "RA "],
+                               ["H2 ", "H3 ", "H4 ", "H5 ", "H6 ", "H7 ", "H8 ", "H9 ", "H10", "HB ", "HQ ", "HK ", "HA "]]
     spelbord = [[], [], [], [], [], [], [], []]
     for i in range(13):
         for j in range(8):
@@ -21,6 +21,7 @@ def schud_kaarten():
 
 def main():
     spelbord = schud_kaarten()
+    
 
 
 if __name__ == "__main__":
