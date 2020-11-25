7c3ac88770dcca69c1bfa9e0e1ab8373380aea2b
IrenvanderBeekPXL <Iren.vanderBeek@student.pxl.be>
2020-11-25 17:42:42 +0100

    Added game list in main program

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 # The games programmed in here do not use the GPU and only 1 core
 
 import os
+games = ["hartenjagen", "memory", "OXO", "quiz", "schaken", "zeeslag"]
 print("Welcome to PyGames")
 print("Some games only work on 1920 * 1080 resolution")
+print("Copy one of the following games")
+for i in games:
+    print(i)
 game = input("Which game do you want to play?")
 while game != "stop":
     print("Opening game...")
     os.system("python3 " + game + "/main.py")
+    print("Copy one of the following games")
+    for i in games:
+        print(i)
     game = input("Which game do you want to play?")
