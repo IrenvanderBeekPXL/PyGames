65539f07a6fde58c727759f7f2b51c69b9c6dc23
IrenvanderBeekPXL <73287294+IrenvanderBeekPXL@users.noreply.github.com>
2020-11-27 16:36:47 +0100

    Added all functiond needed

--- hangman/main.py
+++ hangman/main.py
@@ -1,7 +1,26 @@
-def make_letters_visible(letter, wrong_letters, word, visible_letters):
+def make_found_letters_visible(letter, wrong_letters, word, visible_letters):
     if letter in word:
         for i in range(len(visible_letters)):
-            # need structure to change the letters in visible_letters
-            pass
+            if word[i] == letter:
+                visible_letters[i] = letter
     else:
         wrong_letters.append(letter)
+
+def print_found_letters(visible_letters):
+    for i in visible_letters:
+        print(i, end="")
+
+def hide():
+    input("Press enter to hide")
+    for i in range(2000):
+        print("\n")
+    input("Press enter to continue")
+
+def main():
+    # asking for input and hiding it
+    word = input("Which word?")
+    hide()
+    
+
+if __name__ == "__main__":
+    main()
