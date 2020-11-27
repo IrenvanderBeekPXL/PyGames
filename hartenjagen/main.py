cfc37928525d9ad5677aae23cbdf37e7d62c8266
IrenvanderBeekPXL <73287294+IrenvanderBeekPXL@users.noreply.github.com>
2020-11-27 17:24:19 +0100

    Debugd hartenjagen so it doesn't stick on K2 to start

--- hartenjagen/main.py
+++ hartenjagen/main.py
@@ -71,6 +71,7 @@ def main():
                 strafpunten[picker] += 1
             elif pot[j] == "SQ":
                 strafpunten[picker] += 13
+        starter = picker
     for i in range(starter, starter-4, -1):
         print("player", i, "has", strafpunten[i], "bad points")
 
