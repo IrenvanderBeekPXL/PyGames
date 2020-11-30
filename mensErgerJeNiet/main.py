09db571bdaedd6419261a1c2af30ee0021b49444
IrenvanderBeekPXL <73287294+IrenvanderBeekPXL@users.noreply.github.com>
2020-11-30 18:50:10 +0100

    completed print_board

--- mensErgerJeNiet/main.py
+++ mensErgerJeNiet/main.py
@@ -2,10 +2,13 @@ from secrets import randbelow
 def dice():
     return randbelow(6) + 1
 
-def get_board(board, need_to_start):
+def print_board(board, need_to_start):
     print("   ", end=" ")
     for i in need_to_start:
         print(i, end="  ")
-        for j in range(int(len(board) / len(need_to_start))):
+        for j in range(int(len(board) / len(need_to_start)-1)):
             print("  ", end=" ")
-
+    print("", end="\n ")
+    for i in board:
+        print(i, end="  ")
+print_board([0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0,0 ,0 ,0 ,0, 0, 0], [1, 1, 1])
