1b835f4601971d2627b1ebbbbda4737d5b6e173a
IrenvanderBeekPXL <73287294+IrenvanderBeekPXL@users.noreply.github.com>
2020-11-30 16:12:10 +0100

    added dice and started board

--- mensErgerJeNiet/main.py
+++ mensErgerJeNiet/main.py
@@ -1,1 +1,11 @@
+from secrets import randbelow
+def dice():
+    return randbelow(6) + 1
+
+def get_board(board, need_to_start):
+    print("   ", end=" ")
+    for i in need_to_start:
+        print(i, end="  ")
+        for j in range(int(len(board) / len(need_to_start))):
+            print("  ", end=" ")
 
