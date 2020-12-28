57ea480c2d99eccf37b2bf8c1d1cd7ef7dac471e
Jisu101 <jvdb@iren.be>
2020-12-28 19:44:45 +0100

    Started Kaart.java

--- src/UNO/Kaart.java
+++ src/UNO/Kaart.java
@@ -1,4 +1,26 @@
 package UNO;
 
 public class Kaart {
+    private static final String[] TOEGELATEN_KLEUREN = new String[]{"red", "green", "blue", "yellow", "black"};
+    private static final String[] TOEGELATEN_ICONEN = new String[]{"0", "1","2", "3", "4", "5", "6", "7", "8", "9", "+2", "+4", "WILD", "REVERSE", "SKIP"};
+    private String color;
+    private String icoon;
+
+
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getIcoon() {
+        return icoon;
+    }
+
+    public void setIcoon(String icoon) {
+        this.icoon = icoon;
+    }
 }
