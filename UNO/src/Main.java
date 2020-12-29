9906f39f14f6d3d9cf1179e6d92ed4496b47ccbf
IrenvanderBeekPXL <73287294+IrenvanderBeekPXL@users.noreply.github.com>
2020-12-29 16:23:04 +0100

    started main

--- UNO/src/Main.java
+++ UNO/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        Kaart[] kaarten = new Kaart[]{}; // voeg hier de beschikbare kaarten van het uno spel toe
+        while ()
     }
 }
