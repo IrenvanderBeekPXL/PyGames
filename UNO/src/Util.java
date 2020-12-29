d38deaa947b8fb8469cc8edfbee2649e495425ba
Jisu101 <jvdb@iren.be>
2020-12-29 17:35:25 +0100

    added hide (and seek)

--- UNO/src/Util.java
+++ UNO/src/Util.java
@@ -4,12 +4,18 @@ public abstract class Util {
     add a method to hide the cards (public static void hide())
     */
 
-    public static int getWinner(Speler[] spelers){
+    public static int getWinner(Speler[] spelers) {
         for (Speler speler : spelers) {
             if (speler.getKaarten().toArray().length == 0) {
                 return speler.getSpelerNummer();
             }
         }
         return -1;
+    }
+
+    public static void hide(){
+        for (int i = 0; i < 2000; i++) {
+            System.out.println();
+        }
     }
 }
