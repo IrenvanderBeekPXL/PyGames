ae6eda8be7a94f3eedb30b224225f190ee1cf3a0
IrenvanderBeekPXL <73287294+IrenvanderBeekPXL@users.noreply.github.com>
2020-12-30 14:16:07 +0100

    added type

--- hoogsteKaart/src/Ranking.java
+++ hoogsteKaart/src/Ranking.java
@@ -1,5 +1,5 @@
 public enum Ranking {
-    TWEE("2"), DRIE("3"), VieR("4"), VIJF("5"), ZES("6"), ZEVEN("7"), ACHT("8"),
+    TWEE("2"), DRIE("3"), VIER("4"), VIJF("5"), ZES("6"), ZEVEN("7"), ACHT("8"),
     NEGEN("9"), TIEN("10"), BOER("Boer"), DAME("Dame"), KONING("Koning"), AAS("aas");
     private String naam;
 
---
+++ hoogsteKaart/src/Soort.java
@@ -0,0 +1,10 @@
+public enum Soort {
+    SCHOPPEN("schoppen"), KLAVEREN("klaveren"),
+    HARTEN("harten"), RUITEN("ruiten");
+
+    private String naam;
+
+    Soort(String naam){
+        this.naam = naam;
+    }
+}
