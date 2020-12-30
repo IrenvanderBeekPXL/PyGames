7c9a294867aec250ec07fc4bf900b372871e2d8a
IrenvanderBeekPXL <73287294+IrenvanderBeekPXL@users.noreply.github.com>
2020-12-30 17:09:53 +0100

    added stochfish for windows

--- schaken/main.py
+++ schaken/main.py
@@ -1,2 +1,20 @@
 import os
-os.system("python3 schaken/sunfish-master/sunfish.py")
+from stockfish import Stockfish
+import platform
+#os.system("python3 schaken/sunfish-master/sunfish.py")
+user = "12000780"
+engine = Stockfish("C:/Users/" + user + "/PycharmProjects/PyGames/schaken/stockfish-win/stockfish.exe")
+engine.set_depth(20)
+engine.set_skill_level(20)
+moves = []
+while True:
+    print(engine.get_board_visual(), end="")
+    print("  a   b   c   d   e   f   g   h")
+    print()
+    move = input("Zet een zet zoals e2e4  ")
+    while not engine.is_move_correct(move):
+        move = input("Zet een zet zoals e2e4  ")
+    moves.append(move)
+    engine.set_position(moves)
+    moves.append(engine.get_best_move())
+    engine.set_position(moves)
