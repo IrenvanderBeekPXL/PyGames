6b812348e0e7a953b537dcfa49696ac3f97e611f
IrenvanderBeekPXL <73287294+IrenvanderBeekPXL@users.noreply.github.com>
2021-01-01 12:27:50 +0100

    completed Uno

--- UNO/src/Main.java
+++ UNO/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        boolean normale_volgorde = false;
+        boolean normale_volgorde = true;
         int spelerNummerAanDeBeurt = 0;
         Speler spelerAanDeBeurt = null;
         Kaart[] kaarten = VoegKaartenToe.addKaarten(2, 2, 4);
@@ -10,6 +10,7 @@ public class Main {
         Scanner input = new Scanner(System.in);
         System.out.println("Hoeveel spelers?");
         Speler[] spelers = new Speler[input.nextInt()];
+        String newColor = "";
         for (int i = 0; i < spelers.length; i++) {
             spelers[i] = new Speler(i, kaarten);
         }
@@ -25,6 +26,9 @@ public class Main {
 
             // show the top card
             System.out.println("top card: " + bovensteKaart);
+            if (bovensteKaart.getColor().equals("Black")){
+                System.out.println("Er moet iets op van kleur " + newColor);
+            }
 
             // show the cards
             System.out.println("jouw kaarten:");
@@ -34,30 +38,54 @@ public class Main {
             }
 
             // ask the correct card for input
-            System.out.println("Welke index kaaart wil je? (bovenste is index 1) ");
-            bovensteKaart = spelerAanDeBeurt.popKaart(
-                    Data.minMax(0, spelerAanDeBeurt.getKaarten().toArray().length - 1,
-                            input.nextInt() - 1));
-
-            // doing special stuff if needed
-            // get next player
-            int spelerNummerNext = spelerNummerAanDeBeurt + 1;
-            Speler spelerNext = null;
-            spelerNummerNext %= spelers.length;
-            for (Speler speler : spelers) {
-                if (speler.getSpelerNummer() == spelerNummerNext) {
-                    spelerNext = speler;
-                    break; // less cpu time
+            System.out.println("Welke index kaaart wil je? (bovenste is index 1, 0 is draw) ");
+            try {
+                bovensteKaart = spelerAanDeBeurt.popKaart(
+                        Data.minMax(0, spelerAanDeBeurt.getKaarten().toArray().length - 1,
+                                input.nextInt() - 1));
+                // doing special stuff if needed
+                // get next player
+                int spelerNummerNext = spelerNummerAanDeBeurt + 1;
+                Speler spelerNext = null;
+                spelerNummerNext %= spelers.length;
+                for (Speler speler : spelers) {
+                    if (speler.getSpelerNummer() == spelerNummerNext) {
+                        spelerNext = speler;
+                        break; // less cpu time
+                    }
                 }
+                if (!bovensteKaart.getColor().equals("Black")) {
+                    if (bovensteKaart.getIcoon().equals("+2")) {
+                        spelerNext.drawKaart(kaarten);
+                        spelerNext.drawKaart(kaarten);
+                        spelerNummerAanDeBeurt += 1;
+                    }
+                    if (bovensteKaart.getIcoon().equals("SKIP")) {
+                        spelerNummerAanDeBeurt += 1;
+                    }
+                    if (bovensteKaart.getIcoon().equals("REVERSE")) {
+                        normale_volgorde = !normale_volgorde;
+                    }
+                } else {
+                    System.out.println("Welke kleur? (Blue, Yellow, Green, Red)");
+                    newColor = input.nextLine();
+                    if (bovensteKaart.getIcoon().equals("+4")){
+                        spelerNext.drawKaart(kaarten);
+                        spelerNext.drawKaart(kaarten);
+                        spelerNext.drawKaart(kaarten);
+                        spelerNext.drawKaart(kaarten);
+                        spelerNummerAanDeBeurt += 1;
+                    }
+                }
+            } catch (ArrayIndexOutOfBoundsException exception){
+                spelerAanDeBeurt.drawKaart(kaarten);
+            } catch (NullPointerException | ArithmeticException exception){
+                System.out.println("Er is een fout in de code. Gelieve een bug te reporten " +
+                        "op Github met de volgende informatie:");
+                throw exception;
             }
-            if (bovensteKaart.getIcoon().equals("+2")){
-                spelerNext.drawKaart(kaarten);
-                spelerNext.drawKaart(kaarten);
-                spelerNummerAanDeBeurt += 1;
-            }
-            if (bovensteKaart.getIcoon().equals("SKIP")){
-
-            }
+            spelerNummerAanDeBeurt++;
+            Util.hide();
         }
     }
 }
