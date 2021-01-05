c3dc5413a59aa40c31130b56767887603018a03f
IrenvanderBeekPXL <73287294+IrenvanderBeekPXL@users.noreply.github.com>
2021-01-05 17:01:03 +0100

    started analyzing

--- schaken/main.py
+++ schaken/main.py
@@ -11,26 +11,39 @@ engine.set_skill_level(2)
 wit = True
 moves = []
 engine.set_position(moves)
-input("WARNING. This chess program is about as strong as it gets. Press enter to continue...")
-while engine.get_best_move_time(3000) != None:
-    if wit:
-        print(engine.get_board_visual(), end="")
-        print("  a   b   c   d   e   f   g   h")
-        print()
-        move = input("Zet een zet zoals e2e4  ")
-        while not engine.is_move_correct(move):
+play = input("Do you want to play or load in a game? (play/load) ") == "play"
+if play:
+    input("WARNING. This chess program is about as strong as it gets. Press enter to continue...")
+    while engine.get_best_move_time(3000) != None:
+        if wit:
+            print(engine.get_board_visual(), end="")
+            print("  a   b   c   d   e   f   g   h")
+            print()
             move = input("Zet een zet zoals e2e4  ")
+            while not engine.is_move_correct(move):
+                move = input("Zet een zet zoals e2e4  ")
+            moves.append(move)
+            engine.set_position(moves)
+            wit = False
+        else:
+            engine.set_depth(20)
+            engine.set_skill_level(20)
+            best_move = engine.get_best_move()
+            moves.append(best_move)
+            print("Mijn zet is", best_move)
+            engine.set_position(moves)
+            engine.set_depth(2)
+            engine.set_skill_level(2)
+            wit = True
+else:
+    print("Please give me the series of moves.")
+    print("Press enter with a move to give me the move and \nwithout a move to load in")
+    move = input()
+    while len(move) != 0:
         moves.append(move)
-        engine.set_position(moves)
-        wit = False
-    else:
-        engine.set_depth(20)
-        engine.set_skill_level(20)
-        best_move = engine.get_best_move()
-        moves.append(best_move)
-        print("Mijn zet is", best_move)
-        engine.set_position(moves)
-        engine.set_depth(2)
-        engine.set_skill_level(2)
-        wit = True
-    
+        move = input()
+    engine.set_position(moves)
+
+if input("Do you want to analyze the last game? (Y/n)") == "Y":
+    input("WARNING! Analyzing could take a whole day for a long game. Press enter to continue...")
+
