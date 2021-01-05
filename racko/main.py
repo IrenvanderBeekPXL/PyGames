7c9219c5082a1d89752da6bb6362e4808ecbf467
IrenvanderBeekPXL <73287294+IrenvanderBeekPXL@users.noreply.github.com>
2021-01-05 12:25:30 +0100

    added some utillities and started main

---
+++ racko/main.py
@@ -0,0 +1,12 @@
+import util
+import time
+
+def main():
+    print("Er is een bordje kaarten...")
+    time.sleep(2)
+    print("En de volgorde klopt niet...")
+    time.sleep(2)
+    if not input("Lossen jullie het op? (Y/n) ") == "Y":
+        msg = "Error, players does not want to solve the issue"
+        raise ConnectionRefusedError (msg)
+
---
+++ racko/util.py
@@ -0,0 +1,14 @@
+# TODO
+# voeg een functie toe om een kaart van het bordje te vervangen door een kaart die meegegeven wordt als parameter
+# De index wordt ook meegegeven als parameter en de vorige kaart moet gereturnt worden
+
+
+def hide():
+    input("Press enter to hide")
+    for i in range(2000):
+        print("\n")
+    input("Press enter to continue")
+
+kaarten_stapel = []
+for i in range(60):
+    kaarten_stapel.append(i + 1)
