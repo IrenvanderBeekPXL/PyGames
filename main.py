f613bf43047f6b9bcc782ca8559c5ad495a6b8e4
IrenvanderBeekPXL <73287294+IrenvanderBeekPXL@users.noreply.github.com>
2021-01-16 18:02:18 +0100

    stopped control c'ing out

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ print("Some games only work on 1920 * 1080 resolution")
 print("Copy one of the following games")
 for i in games:
     print(i)
-game = input("Which game do you want to play?")
-while game != "stop":
-    print("Opening game...")
-    if game in pythongames:
-        os.system("python3 " + game + "/main.py")
-    elif game in javagames:
-        os.system("java -jar " + game + "/main.jar")
-    print("Copy one of the following games")
-    for i in games:
-        print(i)
+try:
     game = input("Which game do you want to play?")
+    while game != "stop":
+        print("Opening game...")
+        if game in pythongames:
+            os.system("python3 " + game + "/main.py")
+        elif game in javagames:
+            os.system("java -jar " + game + "/main.jar")
+        print("Copy one of the following games")
+        for i in games:
+            print(i)
+        game = input("Which game do you want to play?")
+except KeyboardInterrupt:
+    print("Please don't Ctrl+C out. Use the stop command instead")
