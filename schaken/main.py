9987cf33fa4ee8be2dd006ed5708bbc6c5a5a75e
IrenvanderBeekPXL <73287294+IrenvanderBeekPXL@users.noreply.github.com>
2021-01-16 16:52:07 +0100

    deleted analys

--- schaken/main.py
+++ schaken/main.py
@@ -13,7 +13,7 @@ engine.set_skill_level(2)
 wit = True
 moves = []
 engine.set_position(moves)
-play = input("Do you want to play or load in a game? (play/load) ") == "play"
+play = True
 if play:
     input("WARNING. This chess program is about as strong as it gets. Press enter to continue...")
     while engine.get_best_move_time(3000) is not None:
@@ -37,91 +37,3 @@ if play:
             engine.set_depth(2)
             engine.set_skill_level(2)
             wit = True
-else:
-    print("Please give me the series of moves.")
-    print("Press enter with a move to give me the move and \nwithout a move to load in")
-    move = input()
-    while len(move) != 0:
-        moves.append(move)
-        move = input()
-    engine.set_position(moves)
-
-if input("Do you want to analyze the last game? (Y/n)") == "Y":
-    input("WARNING! Analyzing could take a whole day for a long game. Press enter to continue...")
-    print("If you run this on a laptop, please plug it in an electricity outlet.")
-    print("Analyzing...")
-    position = []
-    best_moves = []
-    types = []
-    engine.set_depth(25)
-    engine.set_skill_level(20)
-    white = True
-    for i in moves:
-        engine.set_position(position)
-        best_move = engine.get_best_move()
-        best_moves.append(best_move)
-        if i == best_move:
-            print(i, "was the best move")
-            types.append("Best move")
-            position.append(i)
-        else:
-            evaluation_before = engine.get_evaluation()
-            position.append(i)
-            engine.set_position(position)
-            evaluation_after = engine.get_evaluation()
-            if evaluation_after.get("type") == "cp" and evaluation_before.get("type") == "cp":
-                if white:
-                    if evaluation_after.get("value") >= evaluation_before.get("value"):
-                        print(i, "was a good move")
-                        types.append("Good move")
-                    elif evaluation_after.get("value") - 1 >= evaluation_before.get("value"):
-                        types.append("Not that bad")
-                        print(i, "was not so good, but also not bad")
-                    elif evaluation_after.get("value") - 7 >= evaluation_before.get("value"):
-                        types.append("Bad move")
-                        print(i, "was a bad move")
-                    else:
-                        types.append("Blunder")
-                        print(i, "was a blundery blunder")
-                else:
-                    if evaluation_after.get("value") <= evaluation_before.get("value"):
-                        print(i, "was a good move")
-                        types.append("Good move")
-                    elif evaluation_after.get("value") + 1 <= evaluation_before.get("value"):
-                        types.append("Not that bad")
-                        print(i, "was not so good, but also not bad")
-                    elif evaluation_after.get("value") + 7 <= evaluation_before.get("value"):
-                        types.append("Bad move")
-                        print(i, "was a bad move")
-                    else:
-                        types.append("Blunder")
-                        print(i, "was a blundery blunder")
-            elif evaluation_before.get("type") == "mate":
-                print("Forced mate before and after")
-                types.append("Nothing")
-            elif evaluation_after.get("type") == "mate":
-                if not white:
-                    if evaluation_before.get("value") > 60:
-                        types.append("Not that bad")
-                        print(i, "was not so good, but also not bad")
-                    elif evaluation_before.get("value") > 25:
-                        types.append("Bad move")
-                        print(i, "was a bad move")
-                    else:
-                        types.append("Blunder")
-                        print(i, "was a blundery blunder")
-                else:
-                    if evaluation_before.get("value") < -60:
-                        types.append("Not that bad")
-                        print(i, "was not so good, but also not bad")
-                    elif evaluation_before.get("value") < -25:
-                        types.append("Bad move")
-                        print(i, "was a bad move")
-                    else:
-                        types.append("Blunder")
-                        print(i, "was a blundery blunder")
-            else:
-                print("I don't know what is happening with", i)
-                types.append(None)
-
-            print(best_move, "was the best move")
