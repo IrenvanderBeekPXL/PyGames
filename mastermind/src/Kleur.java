92fcb34dd74c9196ee60a80b4f70244676807d71
IrenvanderBeekPXL <73287294+IrenvanderBeekPXL@users.noreply.github.com>
2021-01-19 10:56:58 +0100

    Implemented Kleur

--- mastermind/src/Kleur.java
+++ mastermind/src/Kleur.java
@@ -1,2 +1,14 @@
-public class Kleur {
+public enum Kleur {
+    WIT("Wit"), ROOD("Rood"), GROEN("Groen"), GEEL("Geel"),
+    BLAUW("Blauw"), ZWART("Zwart"), ORANJE("Oranje");
+
+    private final String kleurStr;
+
+    Kleur(String kleurStr){
+        this.kleurStr = kleurStr;
+    }
+
+    public String getKleurStr() {
+        return kleurStr;
+    }
 }
