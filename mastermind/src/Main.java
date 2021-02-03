5d91e237d7378e3a4e8ad969c34af9b03d83a29f
IrenvanderBeekPXL <73287294+IrenvanderBeekPXL@users.noreply.github.com>
2021-02-03 17:28:25 +0100

    started main

--- mastermind/src/Main.java
+++ mastermind/src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        Rij rijNeeded = new Rij();
+        Kleur[] activeRij = new Kleur[4];
+        for (int i = 0; i < 4; i++) {
+            activeRij[i] = Kleur.values()[Data.randomNumber(0, Kleur.values().length - 1)];
+        }
     }
 }
